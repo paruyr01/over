5bd8f29a92cc854783a2b2564c860aa19dfd6566
y-soghomonyan <yervand.odesk@gmail.com>
2024-12-12 21:04:00 +0400

    v2

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,15 +1,13 @@
 window.addEventListener('load', function () {
     const loader = document.getElementById('loader');
-    // const header = document.getElementById('header');
-    // const progressBar = document.getElementById('progress-bar');
+    const progressBar = document.getElementById('progress-bar');
     let progress = 0;
     const interval = setInterval(() => {
         progress += 10; 
-    //     progressBar.style.width = progress + '%';
+        progressBar.style.width = progress + '%';
         if (progress >= 100) {
             clearInterval(interval);
             loader.style.opacity = '0';
-    //         header.classList.add('finish-loader');
             setTimeout(() => {
                 loader.style.display = 'none';
             }, 600); 
