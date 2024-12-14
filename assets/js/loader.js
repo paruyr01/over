cd0b30e45922747c384449ce0e2e7406cd0ae18d
y-soghomonyan <yervand.odesk@gmail.com>
2024-12-14 14:15:11 +0400

    fixes

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,18 +1,20 @@
 window.addEventListener('load', function () {
     const loader = document.getElementById('loader');
-    // const header = document.getElementById('header');
-    // const progressBar = document.getElementById('progress-bar');
+    const header = document.getElementById('header');
+    const progressBar = document.getElementById('progress-bar');
     let progress = 0;
     const interval = setInterval(() => {
-        progress += 10; 
-    //     progressBar.style.width = progress + '%';
-        if (progress >= 100) {
-            clearInterval(interval);
+        progress += 10;
+        if(progressBar) {
+            progressBar.style.width = progress + '%';
+        }
+        if (loader && progress >= 100) {
+            // clearInterval(interval);
             loader.style.opacity = '0';
-    //         header.classList.add('finish-loader');
+            header.classList.add('finish-loader');
             setTimeout(() => {
                 loader.style.display = 'none';
-            }, 600); 
+            }, 600);
         }
     }, 100);
 });
