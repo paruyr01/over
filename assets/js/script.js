e469a559c45e214325d5ef7ef9522d98092bf074
y-soghomonyan <yervand.odesk@gmail.com>
2025-01-18 11:51:56 +0400

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,7 +37,12 @@ function getCountryCode() {
         .then(response => response.json())
         .then(data => {
             // const countryCode = data.country;
-            const countryCode = "BR";
+            const savedLanguage = JSON.parse(localStorage.getItem("selectedLanguage"));
+            let countryCode = "BR";
+            if(savedLanguage && savedLanguage.lang){
+                countryCode = savedLanguage.lang;
+            }
+            
             loadLanguage(countryCode);
         })
         .catch(error => {
@@ -47,3 +52,60 @@ function getCountryCode() {
 }
 
 getCountryCode();
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const dropdownButton = document.querySelector('.dropdown-button');
+    const dropdownMenu = document.querySelector('.dropdown-menu');
+  
+    dropdownButton.addEventListener('click', () => {
+      const isOpen = dropdownMenu.style.display === 'block';
+      dropdownMenu.style.display = isOpen ? 'none' : 'block';
+      dropdownButton.classList.toggle('open', !isOpen);
+    });
+  
+
+    document.addEventListener('click', (event) => {
+      if (!event.target.closest('.custom-dropdown')) {
+        dropdownMenu.style.display = 'none';
+        dropdownButton.classList.remove('open');
+      }
+    });
+  });
+
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const dropdownButton = document.querySelector(".dropdown-button");
+    const dropdownMenu = document.querySelector(".dropdown-menu");
+    const textElement = document.querySelector(".dropdown-button .text");
+    const menuItems = document.querySelectorAll(".dropdown-menu li span");
+
+    const savedLanguage = JSON.parse(localStorage.getItem("selectedLanguage"));
+
+    console.log(savedLanguage);
+    
+    if (savedLanguage) {
+      textElement.textContent = savedLanguage.name;
+    }
+
+    menuItems.forEach((item) => {
+      item.addEventListener("click", (e) => {
+        const selectedLanguage = {
+            name: e.target.textContent,
+            lang: e.target.getAttribute("data-lang"),
+        };
+
+        textElement.textContent = selectedLanguage.name;
+        localStorage.setItem("selectedLanguage", JSON.stringify(selectedLanguage));
+        dropdownMenu.classList.remove("open");
+        location.reload();
+      });
+    });
+  
+    document.addEventListener("click", (e) => {
+      if (!dropdownButton.contains(e.target) && !dropdownMenu.contains(e.target)) {
+        dropdownMenu.classList.remove("open");
+      }
+    });
+  });
